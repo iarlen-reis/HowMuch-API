ccc28efddf9aa9745ed98f1361c34d6daf5e8de6
Iarlen <iarlensr@hotmail.com>
2024-06-06 20:44:58 -0300

    feat: created AuthRepository.

---
+++ app/Repositories/Implementations/AuthRepository.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Repositories\Implementations;
+
+use App\Models\User;
+use App\Repositories\Contracts\AuthRepositoryInterface;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\JsonResponse;
+
+class AuthRepository implements AuthRepositoryInterface
+{
+    public function login(array $credentials): bool | string
+    {
+        return auth()->attempt($credentials);
+    }
+
+    public function register(array $data): User
+    {
+        return User::create($data);
+    }
+
+    public function userExists(string $email): bool
+    {
+        return User::where('email', $email)->exists();
+    }
+
+    public function me(): Authenticatable
+    {
+        return auth()->user();
+    }
+
+    public function logout(): void
+    {
+        auth()->logout();
+    }
+}
