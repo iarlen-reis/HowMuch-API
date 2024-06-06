583cd737490e1d8d5d908d49f627141144aef3b9
Iarlen <iarlensr@hotmail.com>
2024-06-06 20:44:45 -0300

    feat: created AuthRepositoryInterface.

---
+++ app/Repositories/Contracts/AuthRepositoryInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Repositories\Contracts;
+
+use App\Models\User;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\JsonResponse;
+
+interface AuthRepositoryInterface
+{
+    public function login(array $credentials): bool | string;
+
+    public function userExists(string $email): bool;
+
+    public function register(array $data): User;
+
+    public function me(): Authenticatable;
+
+    public function logout(): void;
+}
