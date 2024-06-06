1f46bd99e7cd166bdb95dfaed81a51009e9117a7
Iarlen <iarlensr@hotmail.com>
2024-06-06 20:45:11 -0300

    feat: created AuthService.

---
+++ app/Services/AuthService.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Services;
+
+use App\Http\Resources\AuthResource;
+use App\Repositories\Contracts\AuthRepositoryInterface;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Http\JsonResponse;
+
+class AuthService
+{
+    private $authRepository;
+
+    public function __construct(AuthRepositoryInterface $authRepository)
+    {
+        $this->authRepository = $authRepository;
+    }
+
+    public function login(array $credentials): JsonResponse
+    {
+        if (!$token = $this->authRepository->login($credentials)) {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+
+        return response()->json([
+            'token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL() * 60
+        ]);
+    }
+
+    public function register(array $data): JsonResponse
+    {
+        if ($this->authRepository->userExists($data['email'])) {
+            return response()->json(['message' => 'User already exists.'], 409);
+        }
+
+        $userCreated = $this->authRepository->register($data);
+
+        return response()->json($userCreated, 201);
+    }
+
+    public function me(): Authenticatable
+    {
+        return $this->authRepository->me();
+    }
+
+    public function logout(): void
+    {
+        $this->authRepository->logout();
+    }
+}
