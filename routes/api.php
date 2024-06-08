1b8b63ed634a9039990bff0fb39d4deb38e29896
Iarlen <iarlensr@hotmail.com>
2024-06-08 02:49:44 -0300

    feat: add total next invoices, total current invoice and grouped api routes.

--- routes/api.php
+++ routes/api.php
@@ -16,12 +16,16 @@ Route::group(['prefix' => '/auth', 'middleware' => ['auth:api']], function () {
 });
 
 Route::group(['prefix' => '/invoices', 'middleware' => ['auth:api']], function () {
+    Route::get('/total-next-invoices', [InvoiceController::class, 'totalNextInvoices']);
+    Route::get('/total-current-invoice', [InvoiceController::class, 'totalCurrentInvoice']);
     Route::get('/', [InvoiceController::class, 'index']);
     Route::get('/{id}', [InvoiceController::class, 'show']);
+    Route::get('/{id}/grouped', [InvoiceController::class, 'grouped']);
 });
 
 
 Route::group(['prefix' => '/purchase', 'middleware' => ['auth:api']], function () {
+    Route::get('/last-purchases', [PurchaseController::class, 'lastPurchases']);
     Route::get('/', [PurchaseController::class, 'index']);
     Route::post('/', [PurchaseController::class, 'store']);
     Route::get('/{id}', [PurchaseController::class, 'show']);
