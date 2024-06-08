b3e60fccd60f5b2d6f84b4df4ad46a12d7b650ea
Iarlen <iarlensr@hotmail.com>
2024-06-07 23:45:50 -0300

    feat: add repositories and interfaces on provider app bind.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,11 @@
 namespace App\Providers;
 
 use App\Repositories\Contracts\AuthRepositoryInterface;
+use App\Repositories\Contracts\InvoiceRepositoryInterface;
+use App\Repositories\Contracts\PurchaseRepositoryInterface;
 use App\Repositories\Implementations\AuthRepository;
+use App\Repositories\Implementations\InvoiceRepository;
+use App\Repositories\Implementations\PurchaseRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +17,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(AuthRepositoryInterface::class, AuthRepository::class);
+        $this->app->bind(
+            InvoiceRepositoryInterface::class,
+            InvoiceRepository::class,
+        );
+
+        $this->app->bind(
+            AuthRepositoryInterface::class,
+            AuthRepository::class,
+        );
+
+        $this->app->bind(
+            PurchaseRepositoryInterface::class,
+            PurchaseRepository::class,
+        );
     }
 
     /**
